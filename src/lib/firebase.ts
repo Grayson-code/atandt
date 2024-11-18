791a4a2f08741eecea729880d9408b418ceb2ea3
Grayson-code <northernstarmusic10@gmail.com>
2024-11-18 19:39:26 +0530

    AAAAAAAA

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -4,11 +4,8 @@ import { getFirestore } from 'firebase/firestore';
 import { getAuth } from 'firebase/auth';
 import dotenv from 'dotenv';
 
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
+// !!! : remove this shit
 
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
     apiKey: "AIzaSyDBhhjZpuizdMb6QeUocY-M_KtKyC_VfSE", authDomain: "atandt-75797.firebaseapp.com", projectId: "atandt-75797", storageBucket: "atandt-75797.appspot.com", messagingSenderId: "542974584050", appId: "1:542974584050:web:8bcd4e6b9a053c08809c6b", measurementId: "G-HS36DYLY42"
 };
