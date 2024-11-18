4af8292297d94a6110c67dd31c8f1ec8ef9e92f7
Grayson-code <northernstarmusic10@gmail.com>
2024-11-18 19:39:23 +0530

    major mistakes

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -2,14 +2,15 @@ import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
 import { getFirestore } from 'firebase/firestore';
 import { getAuth } from 'firebase/auth';
-import verystickystuff from "./verystickystuff";
+import dotenv from 'dotenv';
+
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-    ...verystickystuff || process.env.VAR
+    apiKey: "AIzaSyDBhhjZpuizdMb6QeUocY-M_KtKyC_VfSE", authDomain: "atandt-75797.firebaseapp.com", projectId: "atandt-75797", storageBucket: "atandt-75797.appspot.com", messagingSenderId: "542974584050", appId: "1:542974584050:web:8bcd4e6b9a053c08809c6b", measurementId: "G-HS36DYLY42"
 };
 
 // Initialize Firebase
